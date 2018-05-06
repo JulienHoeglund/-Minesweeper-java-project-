671ca843ce6f964a0132bebb3c3a10a9a5b8f396
lefebure <valentin.lefebure@gmail.com>
2018-05-06 19:52:32 +0200

    Implementation of GameWindowTimer class, including the menu buttons PlusTime and MinusTime to adjust remaining time for the incoming game

---
+++ GameWindowTimer.java
@@ -0,0 +1,56 @@
+/*
+The GameWindowTimer allows us to set an end to the game when the timer ( which has been previously set by the player ) hits 0
+* @version 0.1
+* @author Julien Hoeglund, Valentin Lefebure
+*/
+import java.io.*;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+import java.util.*;
+
+
+public class GameWindowTimer extends TimerTask {
+	private GameWindow gW;
+	private JLabel remain;
+	private Grid grid;
+
+	public GameWindowTimer(GameWindow w ,Grid g, JLabel lb){	
+		gW = w;
+		remain = lb;
+		grid = g;
+	}
+
+	private int setTimer(){
+		gW.setTime(gW.getTime()-1);
+		int t = gW.getTime();
+		return t;
+	}
+	@Override
+	public void run(){
+
+		if (gW.getTime()%60 < 10 ){
+			remain.setText("Timer: 0"+Integer.toString(gW.getTime()/60)+":0"+Integer.toString(gW.getTime()%60));
+			
+			
+		}
+		else{
+			remain.setText("Timer: "+Integer.toString(gW.getTime()/60)+":"+Integer.toString(gW.getTime()%60));
+		}
+		this.setTimer();
+
+
+		if (gW.getTime() <  1)
+		{
+			grid.setEnd(false);			
+		}
+		if ( grid.getGameState() == 2){
+			gW.stopTimer();
+		}
+		
+		System.out.println(gW.getTime());
+	}
+
+
+
+}
---
+++ PlusTime.java
@@ -0,0 +1,32 @@
+/**
+* MinusTime is a button which substracts 15 seconds to the game timer   
+*
+* @version 0.1
+* @author Julien Hoeglund , Valentin Lefebure
+*/
+import java.awt.event.*;
+import javax.swing.*;
+import java.awt.*;
+
+public class PlusTime implements ActionListener{
+	private GameWindow w;
+	private JLabel l;
+	private int min;
+	private int sec;
+
+	public PlusTime(GameWindow window, JLabel label){
+		l=label;
+		w=window;
+	}
+	@Override
+	public void actionPerformed(ActionEvent e){
+	
+			w.setTime(w.getTime()+15);
+			min = w.getTime()/60;
+			sec = w.getTime()%60;
+		
+			l.setText("Timer: "+Integer.toString(min) +" min."+Integer.toString(sec) +" sec." );
+	
+		
+	}
+}
